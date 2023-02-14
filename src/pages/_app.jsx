d145a9f93a877b400e85ae59cb1c8fb7a30410af
Matthew Lewis <matthew.a.lewis7@gmail.com>
2023-02-13 20:06:02 -0700

    add analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react'
 
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
+import { Analytics } from '@vercel/analytics/react'
 
 import '@/styles/tailwind.css'
 import 'focus-visible'
@@ -32,6 +33,7 @@ export default function App({ Component, pageProps, router }) {
           <Component previousPathname={previousPathname} {...pageProps} />
         </main>
         <Footer />
+        <Analytics />
       </div>
     </>
   )
