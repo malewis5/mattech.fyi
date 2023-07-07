2a29f1e79835a92779bdb583597fa645b1f00e94
Matthew Lewis <matthew.a.lewis7@gmail.com>
2023-07-07 14:52:14 -0400

    add industry to projects

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -8,6 +8,7 @@ import logoCosmos from '@public/images/logos/peak.svg'
 import logoOpsiq from '@public/images/logos/opsiq.svg'
 import logoRealtor from '@public/images/logos/realtor.svg'
 import logoPlanetaria from '@public/images/logos/peak.svg'
+import logoIndustry from '@public/images/logos/industry.svg'
 
 const projects = [
   {
@@ -16,6 +17,8 @@ const projects = [
       'RevCommerce de-couples the front and back ends into an innovative module concept. As a headless system, you can discreetly optimize one section of your system without disrupting others to make changes faster, easier and less costly.',
     link: { href: 'https://revcommerce.com', label: 'revcommerce.com' },
     logo: logoPlanetaria,
+    stack:
+      'React, Nextjs, Headless CMS, Serverless Functions, Stripe, AWS, MongoDB, Kubernetes, Cloud Native',
   },
   {
     name: "The People's Fire",
@@ -23,6 +26,7 @@ const projects = [
       "The People's Fire is an app designed to bring transparency to philanthropy. It helps users track the flow of donations and see how their contributions are being used. The app aims to increase trust and accountability in the world of charitable giving.",
     link: { href: '#', label: 'Currently under construction' },
     logo: logoCosmos,
+    stack: 'Nextjs, React, Vercel, Prisma, Typescript, tRPC, Postgres, AWS',
   },
   {
     name: 'OPSIQ',
@@ -30,6 +34,7 @@ const projects = [
       'OPSIQ is a comprehensive software for running an inside sales team. It offers a range of features, including real-time reporting, automated lead management, and demo scheduling all powered by a powerful sales algorithm.',
     link: { href: 'https://opsiq.ai', label: 'opsiq.ai' },
     logo: logoOpsiq,
+    stack: 'React, Twilio, Node.js, Express, AWS',
   },
   {
     name: 'Imprint: Learn Visually',
@@ -37,13 +42,25 @@ const projects = [
       "Our mission is to make the world's most important knowledge easy to learn, internalize, and apply to your life. By visualizing and clarifying complex insights from the world's greatest thinkers, we're helping people around the world master essential topics and learn new skills, quickly and easily.",
     link: { href: 'https://imprintapp.com', label: 'imprintapp.com' },
     logo: logoLucid,
+    stack: 'React, Firebase, iOS, Swift, Headless CMS',
   },
   {
     name: 'Realtor.com',
     description:
       'At Realtor.com my team lead the company wide task to improve A11y support on our Flutter app. After finishing, we were promoted to lead support for the entire NewsCorp organization.',
+    stack: 'SwiftUI, iOS, Android, Flutter,',
     link: { href: 'https://realtor.com', label: 'realtor.com' },
     logo: logoRealtor,
+  },
+  {
+    name: 'The Industry App',
+    description:
+      'Still under development - Focused on the Modeling and Acting industry in Manhattan and Los Angeles.',
+    stack: [
+      'React Native, Postgres, Supabase, Serverless Functions, Stripe, AWS',
+    ],
+    link: { href: '#', label: 'Not Available' },
+    logo: logoIndustry,
   },
 ]
 
@@ -52,7 +69,7 @@ function LinkIcon(props) {
     <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
       <path
         d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z"
-        fill="currentColor"
+        fill={props?.fill ?? 'currentColor'}
       />
     </svg>
   )
@@ -90,6 +107,9 @@ export default function Projects() {
                 <Card.Link href={project.link.href}>{project.name}</Card.Link>
               </h2>
               <Card.Description>{project.description}</Card.Description>
+              <Card.Description>
+                <p>{project?.stack}</p>
+              </Card.Description>
               <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
                 <LinkIcon className="h-6 w-6 flex-none" />
                 <span className="ml-2">{project.link.label}</span>
