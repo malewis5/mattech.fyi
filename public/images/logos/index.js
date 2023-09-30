3106a5787fb8af29149603ff8bb6122721c5b6dc
Matthew Lewis <matthew.a.lewis7@gmail.com>
2023-09-29 21:17:32 -0400

    feat: update with new logo

--- public/images/logos/index.js
+++ public/images/logos/index.js
@@ -1,4 +1,4 @@
 export { default as PeakIcon } from './peak.svg'
 export { default as AmazonIcon } from './amazon.svg'
 export { default as RapptrIcon } from './rapptr.svg'
-export { default as MatTechIcon } from './matTech.svg'
+export { default as MatTechIcon } from './mattech.png'
