90439ae87432a70915d1ff459f968feac0f03dd5
malewis5 <matthew.a.lewis7@gmail.com>
2025-02-20 16:46:54 -0500

    init

--- public/images/logos/index.js
+++
@@ -1,5 +0,0 @@
-export { default as PeakIcon } from './peak.svg'
-export { default as AmazonIcon } from './amazon.svg'
-export { default as RapptrIcon } from './rapptr.svg'
-export { default as MatTechIcon } from './mattech.png'
-export { default as VercelIcon } from './vercel.svg'
